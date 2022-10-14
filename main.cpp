2e287fbef951786ef2cf1bc3d8ebdcdbdde19965
HIMANSHU KUAMR SHARMA <61651449+HIMANSHU-SHARMA-I@users.noreply.github.com>
2022-10-15 00:40:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,4 @@
 
 
 //remove the text file and use less things from the code
-
-void keyboard(unsigned char c,int x,int y)
-{
-	if(c)
-	{
-		exit(0);
-	}
-}
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+his program using the console pauser or add your own getch, system("pause") or input loop */
